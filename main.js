e598f5e98ef99cdf6c839abd0ed04d595dd92ad3
nova1751 <nova2319427728@outlook.com>
2023-10-11 15:33:21 +0800

    feat: update the basic log

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("node:path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
-  win.loadURL("https://noa.refrain.cf");
+  win.loadFile("index.html");
 };
 
 app.whenReady().then(() => {
+  ipcMain.handle("ping", () => "pong");
   createWindow();
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
